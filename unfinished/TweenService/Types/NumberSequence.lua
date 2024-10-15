f59f16898506a92b78c3b6c7f4fa15ca30a03aab
daz <71845855+dazscripts@users.noreply.github.com>
2024-10-15 07:37:22 -0500

    TweenService v0.0.2

--- unfinished/TweenService/Types/NumberSequence.lua
+++ unfinished/TweenService/Types/NumberSequence.lua
@@ -1,21 +1,35 @@
 --!native
 
-local squence = {}
+local sequence = {}
 
 function sequence.deconstruct(self, num: NumberSequence)
-    local tbl = {}
-    for i = 1, #num.Keypoints do
-        local point = num.Keypoints[i]
-        tbl[point.Time] = point.Value
-    end
+	local tbl = {}
+	for i = 1, #num.Keypoints do
+		local point = num.Keypoints[i]
+		tbl[point.Time] = point.Value
+	end
+	return tbl
 end
 
 function sequence.reconstruct(self, points)
-    local tbl = {}
-    for i,v in ipairs(points) do
-        table.insert(tbl, NumberSequenceKeypoint.new(i, v))
-    end
-    return NumberSequence.new(tbl)
+	local tbl = {}
+	
+	local points2 = {}
+	
+	for i,v in points do
+		table.insert(points2, i)
+	end
+
+	table.sort(points2, function(a,b)
+		return a<b
+	end)
+	
+	for i,v in ipairs(points2) do
+		local p = points[v]
+		table.insert(tbl, NumberSequenceKeypoint.new(v, p))
+	end
+	
+	return NumberSequence.new(tbl)
 end
 
 return sequence
--- unfinished/TweenService/init.lua
+++ unfinished/TweenService/init.lua
@@ -1,5 +1,7 @@
 --!native
 
+
+
 local tweenService = game:GetService("TweenService")
 local httpService = game:GetService("HttpService")
 local runService = game:GetService("RunService")
@@ -11,41 +13,30 @@ local function lerp(min: number, max: number, alpha: number): number
 	return min + ((max - min) * alpha)
 end
 
-local tweenLibrary = {}
-local activeTweens = {}
-local tweenableTypes = {
-	CFrame = require(script.CFrame),
-	ColorSequence = require(script.ColorSequence),
-	number = require(script.Types.Number),
-	NumberSequence = require(script.Types.NumberSequence),
-	Vector3 = require(script.Vectors).Vector3,
-	Vector2 = require(script.Vectors).Vector2,
+local tweenLibrary = setmetatable({}, {
+	__index = game:GetService("TweenService")
+}) :: TweenService & {
+	Custom: <A, B>(self, oldValue: A, newValue: B, tweenInfo: TweenInfo) -> (customTween)
 }
 
-function tweenLibrary.TweenInfo(
-	time: number, 
-	easingStyle: Enum.EasingStyle?, 
-	easingDirection: Enum.EasingDirection?, 
-	repeatAmounts: number?, 
-	reverse: boolean?, 
-	delay: number?
-)
-	return {
-		Time = time,
-		EasingStyle = easingStyle or Enum.EasingStyle.Sine,
-		EasingDirection = easingDirection or Enum.EasingDirection.Out,
-		RepeatAmounts = repeatAmounts or 0,
-		Reverse = reverse or false,
-		delay = delay or 0,
-	} 
-end
+
+
+local activeTweens = {}
+local tweenableTypes = {
+	--CFrame = require(script.CFrame),
+	--ColorSequence = require(script.ColorSequence),
+	number = require(script.Types.Number),
+	NumberSequence = require(script.Types.NumberSequence),
+	--Vector3 = require(script.Vectors).Vector3,
+	--Vector2 = require(script.Vectors).Vector2,
+}
 
 local methods = {}
-methods.__index = customMethods
-function tweenLibrary.Create(oldValue, newValue, tweenInfo)
+methods.__index = methods
+
+function tweenLibrary:Custom(oldValue, newValue, tweenInfo): customTween
 	assert(typeof(oldValue) == typeof(newValue), "what are you doing")
-	local t = typeof(oldValue)
-	local funcs = tweenableTypes[t]
+	local funcs = tweenableTypes[typeof(oldValue)]
 
 	local self = {
 		oldValue = funcs.deconstruct(nil,oldValue),
@@ -56,8 +47,8 @@ function tweenLibrary.Create(oldValue, newValue, tweenInfo)
 		reconstruct = funcs.reconstruct,
 		equation = easings[tweenInfo.EasingStyle][tweenInfo.EasingDirection],
 		lifetime = 0,
-		loopTimes = tweenInfo.RepeatAmounts,
-		
+		loopTimes = tweenInfo.RepeatCount,
+
 		Completed = signal.new(),
 	}
 
@@ -69,56 +60,65 @@ function methods:OnUpdate(fn)
 	self.OnSteppedReciever = fn
 end
 
+local function checkIfEndTween(self)
+	if self.loopTimes == -1 then
+		self:Pause()
+		self.Completed:Fire()
+	end
+end
+
 local function stepTween(self, dt)
 	local tweenInfo = self.tweenInfo
-	self.lifetime += (tweenInfo.isReversing and -dt) or dt
+	self.lifetime += if self.isReversing then -dt else dt
 
 	local normalizedTime = self.lifetime / tweenInfo.Time
 	local lerpAlpha = self.equation(normalizedTime)
-	
+
 	local currentValue = {}
 	for i,v in self.oldValue do
 		currentValue[i] = lerp(v, self.newValue[i], lerpAlpha)
 	end
-	
+
 	if self.OnSteppedReciever then
 		task.spawn(self.OnSteppedReciever, self:reconstruct(currentValue))
 	end
-	
+
 	if self.isReversing then
 		if normalizedTime <= 0 then
 			self.loopTimes -= 1
 			self.isReversing = false
-			if self.loopTimes == -1 then
-				self:Stop()
-				self.Completed:Fire()
-			end
+			checkIfEndTween(self)
 		end
 	else
 		if normalizedTime >= 1 then
-			if tweenInfo.Reverse then
+			if tweenInfo.Reverses then
 				self.isReversing = true
-			elseif self.loopTimes == -1 then
-				self:Stop()
-				self.Completed:Fire()
 			else
+				self.loopTimes -= 1
+			end
+			
+			if self.loopTimes == -1 then
+				self:Pause()
+				self.Completed:Fire()
+			elseif not self.isReversing then
 				self.lifetime = 0
 			end
 		end
 	end
 end
-.
-function methods:Play(self)
+
+function methods:Play()
 	activeTweens[self.tweenId] = self
 end
 
-function methods:Pause(self)
+function methods:Pause()
 	activeTweens[self.tweenId] = nil
 end
 
-function methods:Cancel(self)
+function methods:Cancel()
 	self:Pause()
-	self.lerpAlpha = 0
+	self.lifetime = 0
+	self.loopTimes = self.tweenInfo.RepeatAmounts
 	task.spawn(self.OnSteppedReciever, self:reconstruct(self.oldValue))
 end
 
@@ -127,5 +127,7 @@ game:GetService("RunService").Heartbeat:Connect(function(dt)
 		stepTween(v, dt)
 	end
 end)
+
+export type customTween = typeof(tweenLibrary:Custom(table.unpack(...)))
 
 return tweenLibrary
